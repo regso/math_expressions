9f673273836a6ecaefe8d4bff080b4468e83cd8d
regso <55665811+regso@users.noreply.github.com>
2024-05-06 23:26:21 +0300

    Init.

---
+++ test/math_expressions_test.dart
@@ -0,0 +1,8 @@
+// import 'package:math_expressions/math_expressions.dart';
+// import 'package:test/test.dart';
+//
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
